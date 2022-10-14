19b716f1c0172ed0cd6613886a5ffd31e88a4f16
nebuuuu <115797464+nebuuuu@users.noreply.github.com>
2022-10-14 23:24:28 +0200

    Add files via upload

---
+++ Pages1/js/JavaScript.js
@@ -0,0 +1,33 @@
+﻿
+
+
+
+var input = document.getElementById("test");
+input.addEventListener("keydown", function(e)
+{
+    if(e.key === "Enter")
+    {
+       document.getElementById("youtube").src = "https://www.youtube.com/embed/" + getId(e.target.value);
+       document.getElementById("test").style.opacity = 0;
+    }
+    
+});
+
+
+function getId(url) {
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+    const match = url.match(regExp);
+
+    return (match && match[2].length === 11)
+      ? match[2]
+      : null;
+
+     
+    }
+
+
+
+
+
+
+
